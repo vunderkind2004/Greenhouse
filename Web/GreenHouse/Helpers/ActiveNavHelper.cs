cdccdca38cbbbded0569db1df4a7a62d12f773dd
vunderkind2004 <litvinov.rd@gmail.com>
2016-04-09 22:37:17 +0300

    Bootstrap is used

---
+++ Web/GreenHouse/Helpers/ActiveNavHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace GreenHouse.Helpers
+{
+    public static class ActiveNavHelper
+    {
+        //public static MvcHtmlString ActionNavLink(this HtmlHelper htmlHelper, HttpRequestBase request, UrlHelper urlHelper, string linkText, string actionName, string controllerName)
+        //{
+        //    var link = htmlHelper.ActionLink(linkText, actionName, controllerName); 
+        //    var url = urlHelper.Action(actionName,controllerName);
+        //    if (request.Url.AbsolutePath.ToLower() == url.ToLower())
+        //    {
+        //        return MvcHtmlString.Create(string.Format("<li>{0}</li>")
+        //    }
+        //        return htmlHelper.ActionLink(linkText, actionName, controllerName, new {@class="active"});
+        //    return htmlHelper.ActionLink(linkText, actionName, controllerName);            
+        //}
+    }
+}
---
+++ Web/GreenHouse/ViewModels/NavLinkViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenHouse.ViewModels
+{
+    public class NavLinkViewModel
+    {
+        public string Name { get; set; }
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public string RequestUrlPath { get; set; }
+    }
+}
